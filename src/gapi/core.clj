a1406dd8a12a49f7bd9b8f46af9b832758ac9e6a
Gal Dolber <gal.dolber@gmail.com>
2015-03-20 04:28:55 -0300

    Cleanup auth

--- src/gapi/core.clj
+++ src/gapi/core.clj
@@ -1,159 +1,56 @@
 (ns gapi.core
-    (:require
-        [clojure.data.json :as json]
-        [clj-http.client :as http]
-        [clojure.string :as string]
-		[gapi.auth :as auth]))
+  (:require
+   [clojure.data.json :as json]
+   [org.httpkit.client :as http]
+   [clojure.string :as string]
+   [gapi.auth :as auth]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;; SETUP ;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def ^:const discovery-url "https://www.googleapis.com/discovery/v1/apis")
+(def cache (atom {}))
 
-;; Forward Declarations
-(declare extract-methods)
-(declare build-ns)
+(defn list-apis []
+  (let [discovery-doc (json/read-json ((http/get discovery-url) :body))]
+    (map #(vector (% :name) (% :version) (% :discoveryRestUrl))
+         (filter #(= (% :preferred) true) (discovery-doc :items)))))
 
-;; The base Discovery Service URL we will process
-(def ^{:private true} discovery_url "https://www.googleapis.com/discovery/v1/apis")
-(def ^{:private true} cache (atom {}))
+(defn get-method-name [name]
+  (let [parts (clojure.string/split name #"\.")]
+    (str (clojure.string/join "." (pop parts)) "/" (last parts))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;  API  ;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn match-params [params func]
+  (let [required (filter (fn [[k v]] (func v)) params)]
+    (map (fn [[k v]] (name k)) required)))
 
-(defn list-apis
-	"Directly eturn a list of APIs available to built, and endpoint discovery document URLs."
-	[]
-    (let [discovery-doc (json/read-json ((http/get discovery_url) :body))]
-        (map #(vector (%1 :name) (%1 :version) (%1 :discoveryRestUrl))
-					(filter #(= (%1 :preferred) true) (discovery-doc :items)))))
+(defn get-path-params [params]
+  (match-params params (fn [p] (= "path" (p :location)))))
 
-(defn build-resource [base r]
-  (reduce
-   (fn [methods [key resource]]
-     (merge methods
-            (extract-methods base resource)
-            (build-resource base resource)))
-   {}
-   (:resources r)))
+(defn get-required-params [params]
+  (match-params params (fn [p] (p :required))))
 
-(defn build
-	"Given a discovery document URL, construct an map of names to functions that 
-	will make the various calls against the resources. Each call accepts a gapi.auth 
-	state map, and list of argument values, an in some cases a JSON encoded body to send 
-	(for write calls)"
-	[api_url]
-        (let [r (json/read-json ((http/get api_url) :body))]
-          (build-resource (:baseUrl r) r)))
+(defn docstring [method]
+  (str (method :description) "\n"
+       "Required parameters: " (string/join " " (get-required-params (method :parameters)))
+       "\n"))
 
-(defn list-methods
-	"List the available methods in a service"
-	[service]
-	(keys service))
+(defn arglists [method]
+  (let [base_args
+        (if (= (method :description) "POST") '[auth parameters body] '[auth parameters])]
+    base_args))
 
-(defn get-doc
-	"Retrieve a docstring for the service call"
-	[service method]
-	((service method) :doc))
+(defn hasreqs? [params args]
+  (reduce #(and % %2) true (map #(contains? args %) (get-required-params params))))
 
-(defn get-scopes
-	"Retrieve a vector of scopes for the service"
-	[service method]
-	((service method) :scopes))
+(defn get-response [res]
+  (if (= (res :status) 200)
+    (json/read-json (res :body))
+    (let [body (json/read-json (res :body))]
+      {:error ((body :error) :message)})))
 
-(defn call
-	"Call a service function"
-	[auth service method & args]
-	(apply ((service method) :fn) auth args))
+(defn get-url [base_url path params args]
+  (str base_url
+       (reduce #(string/replace % (str "{" %2 "}") (args %2)) path params)))
 
-;; Memoized versions of API calls 
-(def ^{:private true} m-list-apis (memoize list-apis))
-(def ^{:private true} m-build (memoize build))
-
-(defn im
-	"Call a service, constructing if necessary"
-	([auth method_name & args]
-		(let [
-			service_name (first (clojure.string/split method_name #"[\.\/]"))
-			api (last (filter #(= (first %1) service_name) (m-list-apis)))
-			service (m-build (last api))]
-			(apply call auth service method_name args))))
-
-(defn anon-im
-	"Call to a service without authentication"
-	[method_name & args]
-	(apply im nil method_name args))
-
-(defn api-ns
-	"Create a namespace for the API calls. TODO: details"
-	([api_url]
-		(api-ns nil api_url))
-	([auth api_url]
-		(let [	service (m-build api_url)
-				build-fn (partial build-ns auth)]
-			(map build-fn service))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;; HELPER METHODS ;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn- build-ns
-	"Create an entry in a namespace for the method"
-	[auth [mname method]]
-	(let [	name (str "gapi." mname)
-			parts (clojure.string/split name #"[\.\/]")
-			namespace (symbol (clojure.string/join "." (pop parts)))]
-				(if (= nil (find-ns namespace)) (create-ns namespace))
-				(intern namespace
-					(with-meta (symbol (last parts)) {:doc (method :doc) :arglists (method :arglists)})
-					(partial (method :fn) auth))
-				name))
-
-(defn- get-method-name
-	"Get a friendly namespace-esque string for the method"
-	[name]
-	(let [parts (clojure.string/split name #"\.")]
-		(str (clojure.string/join "." (pop parts)) "/" (last parts))))
-
-(defn- match-params
-	"Filter the parameters by a given function"
-	[params func]
-	(let [required (filter (fn [[k v]] (func v)) params)]
-		(map (fn [[k v]] (name k)) required)))
-
-(defn- get-path-params
-	"Return a vector of parameter names which appear in the URL path"
-	[params]
-	(match-params params (fn [p] (= "path" (p :location)))))
-
-(defn- get-required-params
-	"Return a vector of required parameter names"
-	[params]
-	(match-params params (fn [p] (p :required))))
-
-(defn- hasreqs?
-	"Determine whether the required params are present in the arguments"
-	[params args]
-	(reduce #(and %1 %2) true (map #(contains? args %1) (get-required-params params))))
-
-(defn- get-response
-	"Check an HTTP response, JSON decoding the body if valud"
-	[res]
-	(if (= (res :status) 200)
-		(json/read-json (res :body))
-		(let [body (json/read-json (res :body))]
-			{:error ((body :error) :message)})))
-
-(defn- get-url
-	"Replace URL path parameters with their values"
-	[base_url path params args]
-	(str base_url
-		(reduce #(string/replace %1 (str "{" %2 "}") (args %2)) path params)))
-
-(defmulti #^{:private true} callfn
-	"Retrieve an anonymous function that makes the proper call for the
-	supplied method description."
-	(fn [base_url method] (method :httpMethod)))
+(defmulti callfn (fn [base_url method] (method :httpMethod)))
 
 (defmacro defcallfn [n fun]
   `(defmethod callfn ~n [base_url# {path# :path method_params# :parameters}]
@@ -171,29 +68,69 @@
 (defcallfn "MOVE" http/move)
 (defcallfn "HEAD" http/head)
 
-(defn- docstring
-	"Return a description for this method"
-	[method]
-	(str (method :description) "\n"
-		"Required parameters: " (string/join " "(get-required-params (method :parameters)))
-		"\n"))
+(defn extract-methods [base_url resource]
+  (reduce
+   (fn [methods [key method]]
+     (assoc methods
+       (get-method-name (:id method))
+       {:fn (callfn base_url method)
+        :doc (docstring method)
+        :arglists (arglists method)
+        :scopes (:scopes method)}))
+   {} (:methods resource)))
 
-(defn- arglists
-	"Return an argument list for the method"
-	[method]
-	(let [base_args
-			(if (= (method :description) "POST") '[auth parameters body] '[auth parameters])]
-		base_args))
+(defn build-resource [base r]
+  (reduce
+   (fn [methods [key resource]]
+     (merge methods
+            (extract-methods base resource)
+            (build-resource base resource)))
+   {}
+   (:resources r)))
 
-(defn- extract-methods
-	"Retrieve all methods from the given resource"
-	[base_url resource]
-	(reduce
-		(fn [methods [key method]]
-			(assoc methods
-				(get-method-name (method :id))
-				{:fn (callfn base_url method)
-				 :doc (docstring method)
-				 :arglists (arglists method)
-				 :scopes (method :scopes)}))
-		{} (resource :methods)))
+(defn build [api_url]
+  (let [r (json/read-json ((http/get api_url) :body))]
+    (build-resource (:baseUrl r) r)))
+
+(def m-list-apis (memoize list-apis))
+(def m-build (memoize build))
+
+(defn call [auth service method & args]
+  (apply ((service method) :fn) auth args))
+
+(defn im
+  ([auth method_name & args]
+     (let [service_name (first (clojure.string/split method_name #"[\.\/]"))
+           api (last (filter #(= (first %1) service_name) (m-list-apis)))
+           service (m-build (last api))]
+       (apply call auth service method_name args))))
+
+(defn anon-im [method_name & args]
+  (apply im nil method_name args))
+
+(defn build-ns [auth [mname method]]
+  (let [name (str "gapi." mname)
+        parts (clojure.string/split name #"[\.\/]")
+        namespace (symbol (clojure.string/join "." (pop parts)))]
+    (if (= nil (find-ns namespace)) (create-ns namespace))
+    (intern namespace
+            (with-meta (symbol (last parts)) {:doc (method :doc) :arglists (method :arglists)})
+            (partial (method :fn) auth))
+    name))
+
+(defn api-ns
+  ([api_url]
+     (api-ns nil api_url))
+  ([auth api_url]
+     (let [service (m-build api_url)
+           build-fn (partial build-ns auth)]
+       (map build-fn service))))
+
+(defn list-methods [service]
+  (keys service))
+
+(defn get-doc [service method]
+  ((service method) :doc))
+
+(defn get-scopes [service method]
+  ((service method) :scopes))
--- test/gapi/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns gapi.test.core
-  (:use [gapi.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
